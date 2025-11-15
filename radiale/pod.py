d03d2aa6865de8ace0e25282dc10a60992164149
xlfe <felixb@gmail.com>
2025-11-15 15:45:48 +1100

    modernising packages

--- radiale/pod.py
+++ radiale/pod.py
@@ -1,47 +1,60 @@
-from . import mdns, esphome, deconz, mqtt, schedule, chromecast
 import asyncio
-import traceback
 import json
 import sys
+import traceback
+
 from boltons.iterutils import remap
-from bcoding import bencode, bdecode
+from fastbencode import bdecode, bencode
+
+from . import chromecast, deconz, esphome, mdns, mqtt, schedule
 
 
 def eprint(e):
-    sys.stderr.buffer.write(e.encode('utf-8') + '\n'.encode('utf-8'))
+    sys.stderr.buffer.write(e.encode("utf-8") + "\n".encode("utf-8"))
     sys.stderr.buffer.flush()
 
 
 def make_clj_code(fn_name):
-    return [{"name": '{}*'.format(fn_name)},
-            {"name": fn_name, "code": """
-(defn """ + fn_name + """
+    return [
+        {"name": "{}*".format(fn_name)},
+        {
+            "name": fn_name,
+            "code": """
+(defn """
+            + fn_name
+            + """
   ([opts cb _])
   ([opts cb]
    (babashka.pods/invoke
     "pod.xlfe.radiale"
-    'pod.xlfe.radiale/""" + fn_name + """*
+    'pod.xlfe.radiale/"""
+            + fn_name
+            + """*
     [opts]
     {:handlers
      {:success (fn [event] (cb (assoc event
                                  :opts (dissoc opts :password :api-key)
-                                 :fn-name :""" + fn_name + """)))
+                                 :fn-name :"""
+            + fn_name
+            + """)))
       :error (fn [{:keys [:ex-message :ex-data]}]
                (binding [*out* *err*]
                  (println "ERROR:" ex-message)))}})
    nil))
-"""}]
+""",
+        },
+    ]
 
 
 def describe_this(fn_names):
     d = [{"name": "astral-now"}, {"name": "sleep-ms"}]
     for fs in fn_names:
         d.extend(make_clj_code(fs))
-    return {"format": "json",
-            "ops": {"shutdown": {}},
-            "namespaces":
-            [{"name": "pod.xlfe.radiale",
-              "vars": d}]}
+    return {
+        "format": "json",
+        "ops": {"shutdown": {}},
+        "namespaces": [{"name": "pod.xlfe.radiale", "vars": d}],
+    }
 
 
 def _write_(d):
@@ -50,12 +63,14 @@ def _write_(d):
 
 
 def clean_data(path, key, value):
-    return value is not float("nan") and \
-           value is not float("inf") and \
-           value is not float("-inf")
+    return (
+        value is not float("nan")
+        and value is not float("inf")
+        and value is not float("-inf")
+    )
 
 
-class OutgoingQ():
+class OutgoingQ:
 
     async def start(self):
         self.running = True
@@ -74,13 +89,13 @@ class OutgoingQ():
     def write_msg(self, id, data, status="status"):
         self.write_raw(
             dict(
-                    value=json.dumps(
-                        remap(data, visit=clean_data)
-                        if type(data) is dict else data
-                        ),
-                    id=id,
-                    status=[status])
-                )
+                value=json.dumps(
+                    remap(data, visit=clean_data) if type(data) is dict else data
+                ),
+                id=id,
+                status=[status],
+            )
+        )
 
 
 class RadialePod(object):
@@ -96,30 +111,34 @@ class RadialePod(object):
         self.out = await OutgoingQ().start()
 
         while self.running:
-            msg = await asyncio.get_event_loop().\
-                    run_in_executor(None, bdecode, sys.stdin.buffer)
+            msg = await asyncio.get_event_loop().run_in_executor(
+                None, bdecode, sys.stdin.buffer
+            )
 
             op = msg["op"]
 
             if op == "describe":
                 self.out.write_raw(
-                        describe_this([
-                            'listen-mdns',
-                            'listen-mqtt',
-                            'listen-deconz',
-                            'subscribe-chromecast',
-                            'millis-solar',
-                            'millis-crontab',
-                            'put-deconz',
-                            'mdns-info',
-                            'subscribe-esp',
-                            'switch-esp',
-                            'service-esp',
-                            'light-esp',
-                            'state-esp'])
-                        )
+                    describe_this(
+                        [
+                            "listen-mdns",
+                            "listen-mqtt",
+                            "listen-deconz",
+                            "subscribe-chromecast",
+                            "millis-solar",
+                            "millis-crontab",
+                            "put-deconz",
+                            "mdns-info",
+                            "subscribe-esp",
+                            "switch-esp",
+                            "service-esp",
+                            "light-esp",
+                            "state-esp",
+                        ]
+                    )
+                )
 
-            elif op == 'shutdown':
+            elif op == "shutdown":
                 self.running = self.out.running = False
 
             elif op == "invoke":
@@ -129,86 +148,79 @@ class RadialePod(object):
                     eprint(traceback.format_exc())
                     ex_type, ex_value, ex_traceback = sys.exc_info()
                     self.out.write_msg(
-                            id=msg["id"],
-                            status="error",
-                            data=repr(ex_value))
+                        id=msg["id"], status="error", data=repr(ex_value)
+                    )
 
     async def invoke(self, msg):
         var = msg["var"]
         id = msg["id"]
         opts = json.loads(msg["args"])[0]
 
-        if var.endswith('sleep-ms'):
-            await asyncio.sleep(int(opts)/1000.0)
+        if var.endswith("sleep-ms"):
+            await asyncio.sleep(int(opts) / 1000.0)
             self.out.write_msg(id=id, status="done", data=opts)
 
-        elif var.endswith('astral-now'):
-            self.out.write_msg(
-                    id=id,
-                    status="done",
-                    data=schedule.astral_now(**opts))
+        elif var.endswith("astral-now"):
+            self.out.write_msg(id=id, status="done", data=schedule.astral_now(**opts))
 
-        elif var.endswith('listen-mdns*'):
-            if 'mdns' not in self.services:
-                self.services['mdns'] = \
-                        await mdns.MDNS().start(self.out)
-            asyncio.create_task(self.services['mdns'].listen(id, opts))
+        elif var.endswith("listen-mdns*"):
+            if "mdns" not in self.services:
+                self.services["mdns"] = await mdns.MDNS().start(self.out)
+            asyncio.create_task(self.services["mdns"].listen(id, opts))
 
-        elif var.endswith('mdns-info*'):
-            assert 'mdns' in self.services
-            asyncio.create_task(self.services['mdns'].info(id, opts))
+        elif var.endswith("mdns-info*"):
+            assert "mdns" in self.services
+            asyncio.create_task(self.services["mdns"].info(id, opts))
 
-        elif var.endswith('listen-deconz*'):
-            assert 'deconz' not in self.services
-            self.services['deconz'] = deconz.Deconz()
-            self.options['deconz'] = opts
-            asyncio.create_task(
-                self.services['deconz'].listen(self.out, id, opts))
+        elif var.endswith("listen-deconz*"):
+            assert "deconz" not in self.services
+            self.services["deconz"] = deconz.Deconz()
+            self.options["deconz"] = opts
+            asyncio.create_task(self.services["deconz"].listen(self.out, id, opts))
 
-        elif var.endswith('put-deconz*'):
-            assert 'deconz' in self.services
-            type_name = opts['type']
-            device_id = opts['id']
-            state = opts['state']
-            opts = self.options['deconz']
+        elif var.endswith("put-deconz*"):
+            assert "deconz" in self.services
+            type_name = opts["type"]
+            device_id = opts["id"]
+            state = opts["state"]
+            opts = self.options["deconz"]
 
             asyncio.create_task(
-                self.services['deconz'].put(
-                    self.out, id, opts,
-                    type_name, device_id, state
-                    ))
+                self.services["deconz"].put(
+                    self.out, id, opts, type_name, device_id, state
+                )
+            )
 
-        elif var.endswith('listen-mqtt*'):
-            asyncio.create_task(
-                    mqtt.mqtt_listen(self.out, id, opts))
+        elif var.endswith("listen-mqtt*"):
+            asyncio.create_task(mqtt.mqtt_listen(self.out, id, opts))
 
-        elif var.endswith('subscribe-chromecast*'):
-            sn = opts['service-name']
+        elif var.endswith("subscribe-chromecast*"):
+            sn = opts["service-name"]
             assert sn
             svc = self.chromecast.get(sn)
 
-            _mdns = self.services.get('mdns')
+            _mdns = self.services.get("mdns")
 
             if svc is None:
                 assert mdns is not None
-                self.chromecast[sn] = \
-                    svc = chromecast.Chromecast(self.out, id, _mdns, sn)
+                self.chromecast[sn] = svc = chromecast.Chromecast(
+                    self.out, id, _mdns, sn
+                )
 
                 await asyncio.create_task(svc.connect())
 
-        elif var.endswith('subscribe-esp*'):
-            sn = opts['service-name']
+        elif var.endswith("subscribe-esp*"):
+            sn = opts["service-name"]
             assert sn
             svc = self.esp.get(sn)
 
-            _mdns = self.services.get('mdns')
+            _mdns = self.services.get("mdns")
 
             # new service, create client
             if svc is None:
 
                 assert mdns is not None
-                self.esp[sn] = \
-                    svc = esphome.ESPHome(self.out, id, _mdns, sn)
+                self.esp[sn] = svc = esphome.ESPHome(self.out, id, _mdns, sn)
 
             # check if its in a connecting loop already
             async with svc.connecting_lock:
@@ -222,31 +234,33 @@ class RadialePod(object):
                 await svc.connected_state(True)
                 await asyncio.create_task(svc.subscribe())
 
-        elif var.endswith('switch-esp*'):
-            sn = opts['service-name']
+        elif var.endswith("switch-esp*"):
+            sn = opts["service-name"]
             service = self.esp.get(sn)
             assert service
-            await service.switch_command(id, opts['key'], opts['state'])
+            await service.switch_command(id, opts["key"], opts["state"])
 
-        elif var.endswith('light-esp*'):
-            sn = opts['service-name']
+        elif var.endswith("light-esp*"):
+            sn = opts["service-name"]
             service = self.esp.get(sn)
             assert service
-            await service.light_command(id, opts['key'], opts['params'])
+            await service.light_command(id, opts["key"], opts["params"])
 
-        elif var.endswith('service-esp*'):
-            sn = opts['service-name']
+        elif var.endswith("service-esp*"):
+            sn = opts["service-name"]
             service = self.esp.get(sn)
             assert service
-            await service.service_command(id, opts['key'], opts['params'])
+            await service.service_command(id, opts["key"], opts["params"])
 
-        elif var.endswith('state-esp*'):
-            sn = opts['service-name']
+        elif var.endswith("state-esp*"):
+            sn = opts["service-name"]
             service = self.esp.get(sn)
             assert service
-            await service.state_update(id, opts['entity_id'], opts['attribute'], opts['state'])
+            await service.state_update(
+                id, opts["entity_id"], opts["attribute"], opts["state"]
+            )
 
-        elif var.endswith('millis-solar*'):
+        elif var.endswith("millis-solar*"):
             ms = schedule.ms_until_solar(opts)
             while ms < 1000:
                 await asyncio.sleep(1)
@@ -254,7 +268,7 @@ class RadialePod(object):
 
             self.out.write_msg(id=id, status="done", data=dict(ms=ms))
 
-        elif var.endswith('millis-crontab*'):
+        elif var.endswith("millis-crontab*"):
             ms = schedule.ms_until_crontab(opts)
             while ms < 1000:
                 await asyncio.sleep(1)
@@ -263,7 +277,7 @@ class RadialePod(object):
             self.out.write_msg(id=id, status="done", data=dict(ms=ms))
 
 
-class ProxyProvider():
+class ProxyProvider:
 
     def __init__(self, out, id, mdns, service_name):
         pass
@@ -282,8 +296,3 @@ class ProxyProvider():
 
     def on_disconnected(self):
         pass
-
-
-
-
-
